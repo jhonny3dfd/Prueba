0d400d15409c7659679d6e211bcb20bf98319fb5
jhonny3dfd <123789631+jhonny3dfd@users.noreply.github.com>
2024-06-27 09:33:06 -0400

    Update Nodo.java

--- Nodo.java
+++ Nodo.java
@@ -1,16 +1,45 @@
 package Simple;
 public class Nodo {
-    public int dato;
+    public String dato;
     public Nodo siguiente; //puntero enlace
    
     //Constructor para instertar al final
-    public Nodo(int d){
+    public Nodo(String d){
         this.dato =d;
         this.siguiente = null;
     }
     //Constructor para insertar al inicio
-    public Nodo (int d, Nodo n){
+    public Nodo (String d, Nodo n){
         dato =d;
         siguiente = n;
+    }
+
+
+    public String getdato() {
+        return dato;
+    }
+    public String getsiguiente() {
+        return siguiente;
+    }
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public nodoSimple getSiguiente() {
+        return siguiente;
+    }
+
+    public void setSiguiente(nodoSimple siguiente) {
+        this.siguiente = siguiente;
     }
 }
