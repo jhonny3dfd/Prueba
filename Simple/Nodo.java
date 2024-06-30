4c1313e19c15cc4af734d320c0a073f7275b4199
jhonny3dfd <123789631+jhonny3dfd@users.noreply.github.com>
2024-06-30 01:13:38 -0400

    Update Nodo.java

--- Simple/Nodo.java
+++ Simple/Nodo.java
@@ -1,55 +1,29 @@
 package Simple;
 
 public class Nodo {
-    private String nombre;
-    private String destino;
-    private String horario;
-    public Nodo siguiente;
 
-    public Nodo(String nombre, String destino, String horario) {
-        this.nombre = nombre;
-        this.destino = destino;
-        this.horario = horario;
+    private Vuelo vuelo;
+    private Nodo siguiente;
+
+    public Nodo(Vuelo vuelo) {
+        this.vuelo = vuelo;
         this.siguiente = null;
     }
 
-    public Nodo(String nombre, String destino, String horario, Nodo n) {
-        this.nombre = nombre;
-        this.destino = destino;
-        this.horario = horario;
-        this.siguiente = n;
+    //Getters and Setters
+    public Vuelo getVuelo() {
+        return vuelo;
     }
 
-    public String getNombre() {
-        return nombre;
-    }
-
-    public String getDestino() {
-        return destino;
-    }
-
-    public String getHorario() {
-        return horario;
+    public void setVuelo(Vuelo vuelo) {
+        this.vuelo = vuelo;
     }
 
     public Nodo getSiguiente() {
         return siguiente;
     }
 
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public void setDestino(String destino) {
-        this.destino = destino;
-    }
-
-    public void setHorario(String horario) {
-        this.horario = horario;
-    }
-
     public void setSiguiente(Nodo siguiente) {
         this.siguiente = siguiente;
     }
-
 }
